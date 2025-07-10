249178f3222398a42dd760c01e029a8b835a408d
raksha703 <703raksha@gmail.com>
2025-07-10 14:13:44 +0530

    added method to prevent backend sleep

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.use(cors({
     credentials : true
 }))
 
+app.get('/api/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"))
